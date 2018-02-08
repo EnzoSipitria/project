43e167b6a643fd1141a3e984975a0c10c60f193a
Antonela Nespoli <antonela.nespoli@gmail.com>
2018-02-08 19:12:21 -0300

    Verificación status ok

--- sauron/src/app/components/carga/carga.component.ts
+++ sauron/src/app/components/carga/carga.component.ts
@@ -11,10 +11,33 @@ import { CAMIONES } from '../../mock/camiones-mock';
 })
 export class CargaComponent implements OnInit {
 
-  constructor() { }
   date: Date = new Date();
   cargas: Carga[];
+
+  constructor() { }
+
   ngOnInit() {
     this.cargas = CAMIONES;
   }
+
+  getStatus(carga): boolean {
+
+    let firstTime;
+    let lastTime;
+    let prop;
+
+    for (prop in carga)
+      if (carga[prop] instanceof Date && !firstTime)
+        firstTime = carga[prop];
+    
+    if (prop === 'llegadaDeposito')
+      lastTime = carga['salidaRDC'];
+    else
+      lastTime = carga[prop];
+
+    if (firstTime && lastTime)
+      return (lastTime.getTime() - firstTime.getTime() > 7200000);
+    else
+      return false;
+  } 
 }
