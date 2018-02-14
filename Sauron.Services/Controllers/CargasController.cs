9526e88d5e137e3e36536253a75b1242cc757cf9
esipitria <esipitria@q4tech.com>
2018-02-14 08:23:44 -0300

    agregado proyecto de servicios, get all, working

---
+++ Sauron.Services/Controllers/CargasController.cs
@@ -0,0 +1,47 @@
+﻿using Sauron.Services.App_Start;
+using Sauron.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sauron.Services.Controllers {
+
+
+    public class CargasController : ApiController {
+        // .. /api/Cargas/All 
+        [HttpGet]
+        public List<CargaModel> All() {
+            DataSet data = SQLConnector.CreateQuery("SELECT * FROM carga");
+            List<CargaModel> cargas = new List<CargaModel>();
+
+            for (int i = 0; i < data.Tables[0].Rows.Count; i++) {
+                CargaModel carga = CargaModel.Map(data.Tables[0].Rows[i]);
+                cargas.Add(carga);
+            }
+            
+            return cargas;
+
+        }
+
+        // .. /api/Cargas/Unfinished 
+        [HttpGet]
+        public List<CargaModel> Unfinished() {
+
+            return null;
+        }
+
+        // api/Cargas/create
+        [HttpPost]
+        public HttpResponseMessage Create([FromBody] CargaModel carga) {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+
+            carga.Insert().Execute();
+            response.Content = new StringContent("Created carga: " + carga.ID);
+            return response;
+        }
+    }
+}
---
+++ Sauron.Services/Models/CamionModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Sauron.Services.Models {
+
+    public class CamionModel {
+
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Conductor { get; set; }
+
+        public static CamionModel Map(DataRow row) {
+
+            object[] rowData = row.ItemArray;
+            return new CamionModel() {
+                ID = (int)rowData[0],
+                Nombre = rowData[1].ToString(),
+                Conductor = rowData[2].ToString()
+            };
+        }
+    }
+}
